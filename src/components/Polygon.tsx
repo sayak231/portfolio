57c0116138bf63e7c437a1d7bae3ca99d2eb8be6
Sayak <sayak649@gmail.com>
2021-12-16 00:07:12 +0530

    change

--- src/components/Polygon.tsx
+++ src/components/Polygon.tsx
@@ -1,13 +1,13 @@
 const Polygon = () => {
   return (
-    <div class="custom-shape-divider-bottom-1639593132">
+    <div className="custom-shape-divider-bottom-1639593132">
       <svg
         data-name="Layer 1"
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1200 120"
         preserveAspectRatio="none"
       >
-        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" class="shape-fill"></path>
+        <path d="M1200 120L0 16.48 0 0 1200 0 1200 120z" className="shape-fill"></path>
       </svg>
     </div>
   );
